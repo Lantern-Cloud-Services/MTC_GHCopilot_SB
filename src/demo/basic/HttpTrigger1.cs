89b8b57782d04a25565c1c6dbd9dc5b65c6554a9
jdvcDev <59450481+jdvcDev@users.noreply.github.com>
2025-01-15 17:34:13 +0000

    revert demo

--- src/demo/basic/HttpTrigger1.cs
+++ src/demo/basic/HttpTrigger1.cs
@@ -9,19 +9,7 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 
-/*
 
-The provided code defines an Azure Function named HttpTrigger1 within the Company.Function namespace. This function is designed to handle HTTP requests and is marked with the [FunctionName("HttpTrigger1")] attribute, which specifies the name of the function. The Run method is the entry point for the function and is defined as public static async Task<IActionResult> Run. It takes two parameters: an HttpRequest object named req and an ILogger object named log.
-
-The function supports both GET and POST HTTP methods, as indicated by the [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] attribute. This attribute also specifies that the function requires function-level authorization.
-
-Inside the Run method, the function logs a message indicating that it has processed a request using the log.LogInformation method. It then attempts to retrieve a query parameter named "name" from the request URL and assigns it to the name variable.
-
-Next, the function reads the entire body of the HTTP request asynchronously using a StreamReader and the ReadToEndAsync method. The request body is then deserialized into a dynamic object using JsonConvert.DeserializeObject. This allows the function to access properties of the JSON payload dynamically. The function extracts the productID, quantity, and orderID properties from the deserialized object, providing a default value of 0 for quantity if it is not present in the request.
-
-Finally, the function returns an OkObjectResult with a response string, indicating that the request was processed successfully. This response will be sent back to the client that made the HTTP request.
-
-*/
 namespace Company.Function
 {
     /// <summary>
